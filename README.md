7d7f70cd80e48b430be1ef0f8ace6d546bb7806d
Yohan Lascombe <ylascombe@octo.com>
2017-09-11 23:15:07 +0200

    Add dynamic call to spark dashboard with env var

---
+++ README.md
@@ -0,0 +1,10 @@
+TODO
+
+# Usage
+
+Spark dashboard url must be defined in a env var named `SPARK_DASHBOARD_URL`.
+Example :
+````
+export SPARK_DASHBOARD_URL="http://<spark_url>/"
+make run
+````
--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"htmlparser/analyser"
 	"github.com/gin-gonic/gin"
+	"os"
+	"net/http"
+	"fmt"
 )
 
 func main() {
 
+
+	// test connectity to spark
+	url := os.Getenv("SPARK_DASHBOARD_URL")
+	_, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println("FATAL : Cannot request spark dashboard :-(, bye bye")
+		os.Exit(-1)
+	}
 
 	router := gin.Default()
 
