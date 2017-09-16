59db4282a4eb9733eaea20e6e55594e72cb9aa34
Yohan Lascombe <ylascombe@octo.com>
2017-09-16 22:26:59 +0200

    Refactor response content write method

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -32,21 +32,21 @@ func Prometheus(c *gin.Context) {
 	// actual_events_per_second_processed
 	sumEvts, _ := stats.Sum(evtsPerBatchs)
 	sumProcessing, _ := stats.Sum(processingTimes)
-	c.String(200, fmt.Sprintf("\nactual_events_per_second_processed %v", sumEvts/sumProcessing))
+
+	str := ""
+	str += fmt.Sprintf("\nactual_events_per_second_processed %v", sumEvts/sumProcessing)
 
 	// actual_events_median_processed
 	medianEvts, _ := stats.Median(evtsPerBatchs)
 	medianProcessing, _ := stats.Median(processingTimes)
-	c.String(200, fmt.Sprintf("\nactual_events_median_processed %v", medianEvts/medianProcessing))
+	str += fmt.Sprintf("\nactual_events_median_processed %v", medianEvts/medianProcessing)
 
-	printPercentiles(c, evtsPerBatchs, "events_per_second", 10)
-	printPercentiles(c, schedulingDelays, "scheduling_delay", 1)
-	printPercentiles(c, processingTimes, "processing_time", 1)
-
-	c.String(200, "\n\t")
+	str += printPercentiles(evtsPerBatchs, "events_per_second", 10)
+	str += printPercentiles(schedulingDelays, "scheduling_delay", 1)
+	str += printPercentiles(processingTimes, "processing_time", 1)
+	str += "\n\t"
+	c.String(200, str)
 }
-
-
 
 func Csv(c *gin.Context) {
 
@@ -57,41 +57,44 @@ func Csv(c *gin.Context) {
 		return
 	}
 
-	c.String(200, "Batch Time,Input Size,Scheduling Delay,Processing Time,Total Delay")
+	str := "Batch Time,Input Size,Scheduling Delay,Processing Time,Total Delay"
 
 	for i := 0; i < len(res.Batches); i++ {
-		c.String(200, fmt.Sprintf("\n%s,%d,%d,%v,%v",
+		str += fmt.Sprintf("\n%s,%d,%d,%v,%v",
 			res.Batches[i].BatchTime,
 			res.Batches[i].InputSize,
 			res.Batches[i].SchedulingDelay,
 			res.Batches[i].ProcessingTime,
 			res.Batches[i].TotalDelay,
-		))
+		)
 	}
 
+	c.String(200, str)
 }
 
-func printPercentiles(c *gin.Context, datas []float64, label string, coeff int) {
+func printPercentiles(datas []float64, label string, coeff int) string {
 	avg, _ := stats.Mean(datas)
 	avg = avg / float64(coeff)
-	c.String(200, fmt.Sprintf("\n%s_avg %v", label, avg))
+	str := fmt.Sprintf("\n%s_avg %v", label, avg)
 	min, _ := stats.Min(datas)
 	min = min / float64(coeff)
-	c.String(200, fmt.Sprintf("\n%s_min %v", label, min))
-	c.String(200, fmt.Sprintf("\n%s_10_percentile %v", label, getPercentile(datas, 10, coeff)))
-	c.String(200, fmt.Sprintf("\n%s_20_percentile %v", label, getPercentile(datas, 20, coeff)))
-	c.String(200, fmt.Sprintf("\n%s_25_percentile %v", label, getPercentile(datas, 25, coeff)))
-	c.String(200, fmt.Sprintf("\n%s_30_percentile %v", label, getPercentile(datas, 30, coeff)))
-	c.String(200, fmt.Sprintf("\n%s_40_percentile %v", label, getPercentile(datas, 40, coeff)))
-	c.String(200, fmt.Sprintf("\n%s_50_percentile %v", label, getPercentile(datas, 50, coeff)))
-	c.String(200, fmt.Sprintf("\n%s_60_percentile %v", label, getPercentile(datas, 60, coeff)))
-	c.String(200, fmt.Sprintf("\n%s_70_percentile %v", label, getPercentile(datas, 70, coeff)))
-	c.String(200, fmt.Sprintf("\n%s_80_percentile %v", label, getPercentile(datas, 80, coeff)))
-	c.String(200, fmt.Sprintf("\n%s_90_percentile %v", label, getPercentile(datas, 90, coeff)))
-	c.String(200, fmt.Sprintf("\n%s_95_percentile %v", label, getPercentile(datas, 95, coeff)))
+	str += fmt.Sprintf("\n%s_min %v", label, min)
+	str += fmt.Sprintf("\n%s_10_percentile %v", label, getPercentile(datas, 10, coeff))
+	str += fmt.Sprintf("\n%s_20_percentile %v", label, getPercentile(datas, 20, coeff))
+	str += fmt.Sprintf("\n%s_25_percentile %v", label, getPercentile(datas, 25, coeff))
+	str += fmt.Sprintf("\n%s_30_percentile %v", label, getPercentile(datas, 30, coeff))
+	str += fmt.Sprintf("\n%s_40_percentile %v", label, getPercentile(datas, 40, coeff))
+	str += fmt.Sprintf("\n%s_50_percentile %v", label, getPercentile(datas, 50, coeff))
+	str += fmt.Sprintf("\n%s_60_percentile %v", label, getPercentile(datas, 60, coeff))
+	str += fmt.Sprintf("\n%s_70_percentile %v", label, getPercentile(datas, 70, coeff))
+	str += fmt.Sprintf("\n%s_80_percentile %v", label, getPercentile(datas, 80, coeff))
+	str += fmt.Sprintf("\n%s_90_percentile %v", label, getPercentile(datas, 90, coeff))
+	str += fmt.Sprintf("\n%s_95_percentile %v", label, getPercentile(datas, 95, coeff))
 	max, _ := stats.Max(datas)
 	max = max / float64(coeff)
-	c.String(200, fmt.Sprintf("\n%s_max %v", label, max))
+	str +=  fmt.Sprintf("\n%s_max %v", label, max)
+
+	return str
 }
 
 func getPercentile(array []float64, percent int, coeff int) float64 {
